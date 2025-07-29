a15961c173a381346bf0f81a079dce116283ab26
lucas <lucasaita4000@gmail.com>
2025-07-29 03:55:11 -0300

    chore: initial commit

---
+++ src/main/java/dev/lucas/MagicChefAI/MagicChefAiApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.MagicChefAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicChefAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicChefAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lucas/MagicChefAI/MagicChefAiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.MagicChefAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicChefAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
