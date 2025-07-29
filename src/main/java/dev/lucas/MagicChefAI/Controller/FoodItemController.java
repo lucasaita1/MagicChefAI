0a580201a11e0a17cb0b46815938832026c3c88b
lucas <lucasaita4000@gmail.com>
2025-07-29 04:41:05 -0300

    chore: Add the application Controller, still incomplete, with temporary mocks

---
+++ src/main/java/dev/lucas/MagicChefAI/Controller/FoodItemController.java
@@ -0,0 +1,29 @@
+package dev.lucas.MagicChefAI.Controller;
+
+import dev.lucas.MagicChefAI.Service.FoodItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/food")
+@RequiredArgsConstructor
+public class FoodItemController {
+
+    private FoodItemService foodItemService;
+
+//CRUD
+
+    //POST
+
+
+    //GET
+
+
+    //UPDATE
+
+
+    //DELETE
+
+}
+
