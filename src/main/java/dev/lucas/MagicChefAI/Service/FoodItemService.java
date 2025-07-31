8903b063a300792f13a15fcb2a2f94253b4d4995
lucas <lucasaita4000@gmail.com>
2025-07-31 05:21:55 -0300

    fix: Fix errors in the Service class by placing private in the Repository class call

--- src/main/java/dev/lucas/MagicChefAI/Service/FoodItemService.java
+++ src/main/java/dev/lucas/MagicChefAI/Service/FoodItemService.java
@@ -5,6 +5,7 @@ import dev.lucas.MagicChefAI.Model.FoodItem;
 import dev.lucas.MagicChefAI.Repository.FoodItemRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,43 +14,36 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class FoodItemService {
 
-    private FoodItemRepository repository;
+    private final FoodItemRepository repository;
 
-    //Metodo criar
-    public FoodItem criarItem (@RequestBody FoodItem foodItem){
+    public FoodItem criarItem(FoodItem foodItem) {
         return repository.save(foodItem);
     }
 
-    // Metodo Listar
-    public List<FoodItem> listarItens (){
+    public List<FoodItem> listarItens() {
         return repository.findAll();
     }
 
-    //Metodo Listar por ID
     public Optional<FoodItem> listarItensPorID(Long id) {
         return repository.findById(id);
     }
 
-    //Metodo Atualizar
-    public FoodItem atualizarItem (@RequestBody FoodItem foodItem, @PathVariable Long id){
-        if (repository.existsById(id)){
+    public FoodItem atualizarItem(FoodItem foodItem, Long id) {
+        if (repository.existsById(id)) {
             foodItem.setId(id);
             return repository.save(foodItem);
         }
         return null;
     }
 
-    //Metodo Deletar
-    public boolean deletarItem (@PathVariable Long id){
-        if (repository.existsById(id)){
+    public boolean deletarItem(Long id) {
+        if (repository.existsById(id)) {
             repository.deleteById(id);
             return true;
         }
         return false;
     }
-
 }
