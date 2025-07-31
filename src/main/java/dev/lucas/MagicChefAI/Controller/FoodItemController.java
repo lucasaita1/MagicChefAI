e9a3ca03f8524bf08abe0fa5ea0e76b8cee35bd8
lucas <lucasaita4000@gmail.com>
2025-07-31 05:20:36 -0300

    fix: Fix errors to Controller by placing private in the Service class call

--- src/main/java/dev/lucas/MagicChefAI/Controller/FoodItemController.java
+++ src/main/java/dev/lucas/MagicChefAI/Controller/FoodItemController.java
@@ -9,58 +9,44 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
 @RestController
-@RequestMapping ("/food")
+@RequestMapping("/food")
 @RequiredArgsConstructor
 public class FoodItemController {
 
-    private FoodItemService foodItemService;
+    private final FoodItemService foodItemService;
 
-//CRUD
-
-    //POST
-    @PostMapping ("/criar")
-    public ResponseEntity<FoodItem> criandoItem (@RequestBody FoodItem foodItem){
+    @PostMapping("/criar")
+    public ResponseEntity<FoodItem> criandoItem(@RequestBody FoodItem foodItem) {
         return ResponseEntity.status(HttpStatus.CREATED).body(foodItemService.criarItem(foodItem));
     }
 
-    //GET
-    @GetMapping ("/listar")
-    public ResponseEntity<List<FoodItem>> listandoItens (){
+    @GetMapping("/listar")
+    public ResponseEntity<List<FoodItem>> listandoItens() {
         return ResponseEntity.ok(foodItemService.listarItens());
     }
 
-    @GetMapping ("/listar/{id}")
+    @GetMapping("/listar/{id}")
     public ResponseEntity<FoodItem> listandoPorId(@PathVariable Long id) {
         return foodItemService.listarItensPorID(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-
-    //UPDATE
-    @PutMapping ("/atualizar/{id}")
+    @PutMapping("/atualizar/{id}")
     public ResponseEntity<FoodItem> atualizandoItem(@RequestBody FoodItem foodItem, @PathVariable Long id) {
         Optional<FoodItem> existente = foodItemService.listarItensPorID(id);
-
         if (existente.isPresent()) {
             FoodItem itemAtualizado = foodItemService.atualizarItem(foodItem, id);
             return ResponseEntity.ok(itemAtualizado);
         }
-
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-
-
-    //DELETE
-    @DeleteMapping
-    public ResponseEntity<FoodItem> deletandoItem (@PathVariable Long id){
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<Void> deletandoItem(@PathVariable Long id) {
         foodItemService.deletarItem(id);
         return ResponseEntity.noContent().build();
     }
-
-
 }
 
