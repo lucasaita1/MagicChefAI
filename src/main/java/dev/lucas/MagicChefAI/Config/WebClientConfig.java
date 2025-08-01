9790cc5698444147eef325c105bcd09b324da638
lucas <lucasaita4000@gmail.com>
2025-08-01 05:10:40 -0300

    feat: Add WebFlux configuration

---
+++ src/main/java/dev/lucas/MagicChefAI/Config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package dev.lucas.MagicChefAI.Config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Value("${chatgpt.api.url:https://api.openai.com/v1/chat/completions}")
+    private String chatGptURL;
+
+    public WebClient webClient (WebClient.Builder builder){
+        return builder.baseUrl(chatGptURL).build();
+    }
+}
