a21eb0deff9cf196ffcf791278d9f3d2101b200e
lucas <lucasaita4000@gmail.com>
2025-08-02 04:10:09 -0300

    fix: fix API initialization error due to forgetting the @Bean annotation in the WebClienteConfig class

--- src/main/java/dev/lucas/MagicChefAI/Config/WebClientConfig.java
+++ src/main/java/dev/lucas/MagicChefAI/Config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package dev.lucas.MagicChefAI.Config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,6 +11,7 @@ public class WebClientConfig {
     @Value("${chatgpt.api.url:https://api.openai.com/v1/chat/completions}")
     private String chatGptURL;
 
+    @Bean
     public WebClient webClient (WebClient.Builder builder){
         return builder.baseUrl(chatGptURL).build();
     }
