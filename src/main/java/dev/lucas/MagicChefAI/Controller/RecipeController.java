4354bfc5f0e2b13269191bd1bf994e3958370143
lucas <lucasaita4000@gmail.com>
2025-08-02 05:46:12 -0300

    feat: adds the list of database items to the route, thus completing the integration of the DB with the AI

--- src/main/java/dev/lucas/MagicChefAI/Controller/RecipeController.java
+++ src/main/java/dev/lucas/MagicChefAI/Controller/RecipeController.java
@@ -1,5 +1,7 @@
 package dev.lucas.MagicChefAI.Controller;
 
+import dev.lucas.MagicChefAI.Model.FoodItem;
+import dev.lucas.MagicChefAI.Service.FoodItemService;
 import dev.lucas.MagicChefAI.Service.OpenAiService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,15 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 public class RecipeController {
 
     private final OpenAiService openAiService;
+    private final FoodItemService service;
 
     @GetMapping("/generate")
     public Mono<ResponseEntity<String>> generationRecipe(){
-        return openAiService.generationRecipe()
+        List<FoodItem> foodItems = service.listarItens();
+        return openAiService.generationRecipe(foodItems)
                 .map(recipe -> ResponseEntity.ok(recipe))
                 .defaultIfEmpty(ResponseEntity.noContent().build());
 
